b08ceed9f00ef8bbb5ce1ff08fc5610ba6a952e5
MiguelSP040 <20213tn040@utez.edu.mx>
2024-07-29 14:44:47 -0600

    Actualización DaoProduct y DaoProvider

--- src/main/java/com/utez/edu/almacen/models/product/DaoProduct.java
+++ src/main/java/com/utez/edu/almacen/models/product/DaoProduct.java
@@ -1,6 +1,128 @@
 package com.utez.edu.almacen.models.product;
 
+import com.utez.edu.almacen.models.area.DaoArea;
+import com.utez.edu.almacen.models.user.DaoUser;
 import com.utez.edu.almacen.templates.DaoTemplate;
+import com.utez.edu.almacen.utils.MySQLConnection;
 
-public class DaoProduct {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DaoProduct implements DaoTemplate<BeanProduct>{
+    private Connection conn = new MySQLConnection().getConnection();
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    @Override
+    public List<BeanProduct> listAll() {
+        List<BeanProduct> products = null;
+        try {
+            products = new ArrayList<>();
+            String query = "SELECT * FROM products;";
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                BeanProduct product = new BeanProduct();
+                product.setId_product(rs.getLong("id_product"));
+                product.setName(rs.getString("name"));
+                product.setDescription(rs.getString("description"));
+                product.setCode(rs.getString("code"));
+                products.add(product);
+            }
+        }catch (SQLException e){
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, "ERROR. Function listAll failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return products;
+    }
+
+    @Override
+    public BeanProduct listOne(Long id) {
+        try {
+            String query = "SELECT * FROM products WHERE id_product = ?";
+            ps = conn.prepareStatement(query);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
+            BeanProduct product = new BeanProduct();
+            if (rs.next()){
+                product.setId_product(rs.getLong("id_product"));
+                product.setName(rs.getString("name"));
+                product.setDescription(rs.getString("description"));
+                product.setCode(rs.getString("code"));
+            }
+            return product;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, "ERROR. Function listOne failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean save(BeanProduct object) {
+        try {
+            String query = "INSERT INTO products(name, description, code) VALUES(?, ?, ?);";
+            ps = conn.prepareStatement(query);
+            ps.setString(1, object.getName());
+            ps.setString(2, object.getDescription());
+            ps.setString(3, object.getCode());
+            return ps.executeUpdate() > 0;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, "ERROR. Function save failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(BeanProduct object) {
+        try {
+            String query = "UPDATE products SET name = ?, description = ?, code = ? WHERE id_product = ?;";
+            ps = conn.prepareStatement(query);
+            ps.setString(1, object.getName());
+            ps.setString(2, object.getDescription());
+            ps.setString(3, object.getCode());
+            ps.setLong(4, object.getId_product());
+            return ps.executeUpdate() > 0;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, "ERROR. Function update failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        try {
+            String query = "DELETE FROM products WHERE id_product = ?;";
+            ps = conn.prepareStatement(query);
+            ps.setLong(1, id);
+            return ps.executeUpdate() == 1;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, "ERROR. Function delete failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    public void closeConnection(){
+        try {
+            if (conn != null) conn.close();
+            if (ps != null) conn.close();
+            if (rs != null) conn.close();
+        }catch (SQLException e) {
+            Logger.getLogger(DaoUser.class.getName()).log(Level.SEVERE, "ERROR. Function closeConnection failed" + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/utez/edu/almacen/models/provider/DaoProvider.java
+++ src/main/java/com/utez/edu/almacen/models/provider/DaoProvider.java
@@ -1,4 +1,153 @@
 package com.utez.edu.almacen.models.provider;
 
-public class DaoProvider {
+import com.utez.edu.almacen.models.user.DaoUser;
+import com.utez.edu.almacen.templates.DaoTemplate;
+import com.utez.edu.almacen.utils.MySQLConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DaoProvider implements DaoTemplate<BeanProvider> {
+    private Connection conn = new MySQLConnection().getConnection();
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    @Override
+    public List<BeanProvider> listAll() {
+        List<BeanProvider> providers = null;
+        try {
+            providers = new ArrayList<>();
+            String query = "SELECT * FROM providers;";
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                BeanProvider provider = new BeanProvider();
+                provider.setId_provider(rs.getLong("id_provider"));
+                provider.setName(rs.getString("name"));
+                provider.setSocialCase(rs.getString("socialCase"));
+                provider.setRfc(rs.getString("rfc"));
+                provider.setPostCode(rs.getString("postCode"));
+                provider.setAddress(rs.getString("address"));
+                provider.setPhone(rs.getString("phone"));
+                provider.setEmail(rs.getString("email"));
+                provider.setContactName(rs.getString("contactName"));
+                provider.setContactPhone(rs.getString("contactPhone"));
+                providers.add(provider);
+            }
+        }catch (SQLException e){
+            Logger.getLogger(DaoProvider.class.getName()).log(Level.SEVERE, "ERROR. Function listAll failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return providers;
+    }
+
+    @Override
+    public BeanProvider listOne(Long id) {
+        try {
+            String query = "SELECT * FROM providers WHERE id_provider = ?;";
+            ps = conn.prepareStatement(query);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
+            BeanProvider provider = new BeanProvider();
+            if (rs.next()){
+                provider.setId_provider(rs.getLong("id_provider"));
+                provider.setName(rs.getString("name"));
+                provider.setSocialCase(rs.getString("socialCase"));
+                provider.setRfc(rs.getString("rfc"));
+                provider.setPostCode(rs.getString("postCode"));
+                provider.setAddress(rs.getString("address"));
+                provider.setPhone(rs.getString("phone"));
+                provider.setEmail(rs.getString("email"));
+                provider.setContactName(rs.getString("contactName"));
+                provider.setContactPhone(rs.getString("contactPhone"));
+            }
+            return provider;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProvider.class.getName()).log(Level.SEVERE, "ERROR. Function listOne failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean save(BeanProvider object) {
+        try {
+            String query = "INSERT INTO providers(name, socialCase, rfc, postCode, address, phone, email, contactName, contactPhone)" +
+                    "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);";
+            ps = conn.prepareStatement(query);
+            ps.setString(1, object.getName());
+            ps.setString(2, object.getSocialCase());
+            ps.setString(3, object.getRfc());
+            ps.setString(4, object.getPostCode());
+            ps.setString(5, object.getAddress());
+            ps.setString(6, object.getPhone());
+            ps.setString(7, object.getEmail());
+            ps.setString(8, object.getContactName());
+            ps.setString(9, object.getContactPhone());
+            return  ps.executeUpdate() > 0;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProvider.class.getName()).log(Level.SEVERE, "ERROR. Function save failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(BeanProvider object) {
+        try {
+            String query = "UPDATE providers SET name = ?, socialCase = ?, rfc = ?, postCode = ?, address = ?," +
+                    "phone = ?, email = ?, contactName = ?, contactPhone = ? WHERE id_provider = ?";
+            ps = conn.prepareStatement(query);
+            ps.setString(1, object.getName());
+            ps.setString(2, object.getSocialCase());
+            ps.setString(3, object.getRfc());
+            ps.setString(4, object.getPostCode());
+            ps.setString(5, object.getAddress());
+            ps.setString(6, object.getPhone());
+            ps.setString(7, object.getEmail());
+            ps.setString(8, object.getContactName());
+            ps.setString(9, object.getContactPhone());
+            ps.setLong(10, object.getId_provider());
+            return ps.executeUpdate() > 0;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProvider.class.getName()).log(Level.SEVERE, "ERROR. Function update failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        try {
+            String query = "DELETE * FROM providers WHERE id_provider;";
+            ps = conn.prepareStatement(query);
+            ps.setLong(1, id);
+            return ps.executeUpdate() == 1;
+        }catch (SQLException e){
+            Logger.getLogger(DaoProvider.class.getName()).log(Level.SEVERE, "ERROR. Function update failed" + e.getMessage());
+        }finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    public void closeConnection(){
+        try {
+            if (conn != null) conn.close();
+            if (ps != null) conn.close();
+            if (rs != null) conn.close();
+        }catch (SQLException e) {
+            Logger.getLogger(DaoUser.class.getName()).log(Level.SEVERE, "ERROR. Function closeConnection failed" + e.getMessage());
+        }
+    }
 }
