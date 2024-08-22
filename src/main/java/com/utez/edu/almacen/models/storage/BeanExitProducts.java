677a6b4809cebfac478ac8a5f429a332a5bb4bab
MiguelSP040 <20213tn040@utez.edu.mx>
2024-08-22 17:35:19 -0600

    [ADD] BeanExitProduct [UPDATE] dMySQLConnection, cambio de BD

---
+++ src/main/java/com/utez/edu/almacen/models/storage/BeanExitProducts.java
@@ -0,0 +1,70 @@
+package com.utez.edu.almacen.models.storage;
+
+public class BeanExitProducts {
+    private Long idProductExit;
+    private Long idExit;
+    private Long idProduct;
+    private int quantity;
+    private double unitPrice;
+    private double totalPrice;
+
+    public BeanExitProducts() {
+    }
+
+    public BeanExitProducts(Long idProductExit, Long idExit, Long idProduct, int quantity, double unitPrice, double totalPrice) {
+        this.idProductExit = idProductExit;
+        this.idExit = idExit;
+        this.idProduct = idProduct;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getIdProductExit() {
+        return idProductExit;
+    }
+
+    public void setIdProductExit(Long idProductExit) {
+        this.idProductExit = idProductExit;
+    }
+
+    public Long getIdExit() {
+        return idExit;
+    }
+
+    public void setIdExit(Long idExit) {
+        this.idExit = idExit;
+    }
+
+    public Long getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Long idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
